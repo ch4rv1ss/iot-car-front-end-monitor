5df93160efcf06980869650d4f60a8dfb4ebab2f
ch4rv1ss <xavirs10@gmail.com>
2024-05-16 14:11:21 -0600

    Add files via upload

---
+++ monitor/js/main.js
@@ -0,0 +1,31 @@
+//alert("HOLA MUNDO")
+
+let mensaje = document.getElementById("respuesta");
+
+function callApiRequest(){
+axios
+  .get('http://localhost/iot-car-control/back-end/apis/getRegistro.php')
+  .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response);
+
+    let respuestaServidor = 
+    response.data == 'f'?"ADELANTE":
+    response.data == 'b'?"ATRÁS":
+    response.data == 'l'?"IZQUIERDA":
+    response.data == 'r'?"DERECHA":
+    "DETENER";
+
+    respuesta.innerHTML = "Última Orden: <strong> "+respuestaServidor + "</strong>";
+  })
+  .catch(function (error) {
+    // manejar error
+    console.log(error);
+  })
+  .finally(function () {
+    // siempre sera executado
+  });
+}
+
+setInterval(callApiRequest, 2000);
+
